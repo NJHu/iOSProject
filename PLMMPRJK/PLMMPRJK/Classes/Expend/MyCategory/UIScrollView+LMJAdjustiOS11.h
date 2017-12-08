ba171df9aa393a386fe18a6a091857c65e5ab8a9
胡鹏 <64hp@163.com>
2017-12-08 11:35:45 +0800

    scrollviewbug

---
+++ PLMMPRJK/PLMMPRJK/Classes/Expend/MyCategory/UIScrollView+LMJAdjustiOS11.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+LMJAdjustiOS11.h
+//  PLMMPRJK
+//
+//  Created by HuXuPeng on 2017/12/8.
+//  Copyright © 2017年 GoMePrjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (LMJAdjustiOS11)
+
+@end
---
+++ PLMMPRJK/PLMMPRJK/Classes/Expend/MyCategory/UIScrollView+LMJAdjustiOS11.m
@@ -0,0 +1,14 @@
+//
+//  UIScrollView+LMJAdjustiOS11.m
+//  PLMMPRJK
+//
+//  Created by HuXuPeng on 2017/12/8.
+//  Copyright © 2017年 GoMePrjk. All rights reserved.
+//
+
+#import "UIScrollView+LMJAdjustiOS11.h"
+
+@implementation UIScrollView (LMJAdjustiOS11)
+
+
+@end
