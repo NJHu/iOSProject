3c7ed26a387e5a6c63f2f2c0e3272a026c82ecd4
胡鹏 <64hp@163.com>
2017-12-08 11:41:51 +0800

    处理 ios11 bug

--- PLMMPRJK/PLMMPRJK/Classes/Expend/MyCategory/UIScrollView+LMJAdjustiOS11.m
+++ PLMMPRJK/PLMMPRJK/Classes/Expend/MyCategory/UIScrollView+LMJAdjustiOS11.m
@@ -10,5 +10,18 @@
 
 @implementation UIScrollView (LMJAdjustiOS11)
 
++ (void)load {
+    
+    method_exchangeImplementations(class_getInstanceMethod(self, @selector(setDelegate:)), class_getInstanceMethod(self, @selector(set_swDelegate:)));
+}
+
+- (void)set_swDelegate:(id<UIScrollViewDelegate>)delegate
+{
+    [self set_swDelegate:delegate];
+    
+    if (@available(iOS 11.0, *)) {
+        self.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+}
 
 @end
