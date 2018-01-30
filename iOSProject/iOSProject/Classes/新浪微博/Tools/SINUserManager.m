e2763b0ad79a4a0600a1d18af03f9541b9f06f60
胡鹏 <64hp@163.com>
2018-01-30 16:26:45 +0800

    update

--- iOSProject/iOSProject/Classes/新浪微博/Tools/SINUserManager.m
+++ iOSProject/iOSProject/Classes/新浪微博/Tools/SINUserManager.m
@@ -111,12 +111,16 @@
 {
     self = [super init];
     if (self) {
+
         // 测试作者信息, 自己获取请注释
-        _name = @"NJ影伴人久";
-        _accessToken = @"2.00mNwvuFZJms7E3e295670faFpUfCE";
-        _iconurl = @"https://tvax3.sinaimg.cn/crop.1.0.510.510.180/005XyiFAly8fescv0z62zj30e80e6q3o.jpg";
-        _uid = @"5460642906";
-        _expiration = [NSDate distantFuture];
+        if ([LMJThirdSDKSinaAppKey isEqualToString:@"4061770881"]) {
+            _name = @"NJ影伴人久";
+            _accessToken = @"2.00mNwvuFZJms7E3e295670faFpUfCE";
+            _iconurl = @"https://tvax3.sinaimg.cn/crop.1.0.510.510.180/005XyiFAly8fescv0z62zj30e80e6q3o.jpg";
+            _uid = @"5460642906";
+            _expiration = [NSDate distantFuture];
+        }
+
     }
     return self;
 }
