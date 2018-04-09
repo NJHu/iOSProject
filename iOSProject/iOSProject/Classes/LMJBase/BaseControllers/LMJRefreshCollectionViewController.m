1e28b4f03b2cb75e4e0deda8230a7d62daca1732
胡鹏 <64hp@163.com>
2018-04-09 18:57:28 +0800

    refreshing bug fix

--- iOSProject/iOSProject/Classes/LMJBase/BaseControllers/LMJRefreshCollectionViewController.m
+++ iOSProject/iOSProject/Classes/LMJBase/BaseControllers/LMJRefreshCollectionViewController.m
@@ -35,10 +35,20 @@
 {
     // 控制只能下拉或者上拉
     if (isMore) {
-        ![self.collectionView.mj_header isRefreshing] ?: [self.collectionView.mj_header endRefreshing];
+        if ([self.collectionView.mj_header isRefreshing]) {
+            [self.collectionView.mj_footer endRefreshing];
+            return;
+        }
+        self.collectionView.mj_header.hidden = YES;
+        self.collectionView.mj_footer.hidden = NO;
     }else
     {
-        ![self.collectionView.mj_footer isRefreshing] ?: [self.collectionView.mj_footer endRefreshing];
+        if ([self.collectionView.mj_footer isRefreshing]) {
+            [self.collectionView.mj_header endRefreshing];
+            return;
+        }
+        self.collectionView.mj_header.hidden = NO;
+        self.collectionView.mj_footer.hidden = YES;
     }
     [self loadMore:isMore];
 }
@@ -49,6 +59,8 @@
     // 结束刷新状态
     ![self.collectionView.mj_header isRefreshing] ?: [self.collectionView.mj_header endRefreshing];
     ![self.collectionView.mj_footer isRefreshing] ?: [self.collectionView.mj_footer endRefreshing];
+    self.collectionView.mj_header.hidden = NO;
+    self.collectionView.mj_footer.hidden = NO;
 }
 
 
--- iOSProject/iOSProject/Classes/LMJBase/BaseControllers/LMJRefreshTableViewController.m
+++ iOSProject/iOSProject/Classes/LMJBase/BaseControllers/LMJRefreshTableViewController.m
@@ -35,10 +35,20 @@
 {
     // 控制只能下拉或者上拉
     if (isMore) {
-        ![self.tableView.mj_header isRefreshing] ?: [self.tableView.mj_header endRefreshing];
+        if ([self.tableView.mj_header isRefreshing]) {
+            [self.tableView.mj_footer endRefreshing];
+            return;
+        }
+        self.tableView.mj_header.hidden = YES;
+        self.tableView.mj_footer.hidden = NO;
     }else
     {
-        ![self.tableView.mj_footer isRefreshing] ?: [self.tableView.mj_footer endRefreshing];
+        if ([self.tableView.mj_footer isRefreshing]) {
+            [self.tableView.mj_header endRefreshing];
+            return;
+        }
+        self.tableView.mj_header.hidden = NO;
+        self.tableView.mj_footer.hidden = YES;
     }
     [self loadMore:isMore];
 }
@@ -51,7 +61,8 @@
     // 结束刷新状态
     ![self.tableView.mj_header isRefreshing] ?: [self.tableView.mj_header endRefreshing];
     ![self.tableView.mj_footer isRefreshing] ?: [self.tableView.mj_footer endRefreshing];
-    
+    self.tableView.mj_header.hidden = NO;
+    self.tableView.mj_footer.hidden = NO;
 }
 
 // 子类需要调用调用
