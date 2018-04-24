b491a1194cc0d2f8ec95c85f74a3bb1fa0d1183b
胡鹏 <64hp@163.com>
2018-04-24 09:07:58 +0800

    update readme

--- iOSProject/iOSProject/Classes/新浪微博/Tools/SINUserManager.m
+++ iOSProject/iOSProject/Classes/新浪微博/Tools/SINUserManager.m
@@ -100,7 +100,7 @@
         // 测试作者信息, 自己获取请注释
         if ([LMJThirdSDKSinaAppKey isEqualToString:@"4061770881"]) {
             _name = @"NJ影伴人久";
-            _accessToken = @"2.00afSYxFZJms7E8e40f0acedp4pUnD";
+            _accessToken = @"2.00afSYxFZJms7Eb7fba204741DuUPB";
             _iconurl = @"https://tvax3.sinaimg.cn/crop.1.0.510.510.180/005XyiFAly8fescv0z62zj30e80e6q3o.jpg";
             _uid = @"5460642906";
             _expiration = [NSDate distantFuture];
