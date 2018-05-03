6ececb01be75ab52396c964856ffbfc5a2648df7
胡鹏 <64hp@163.com>
2018-05-03 14:23:34 +0800

    bug fix

--- iOSProject/Pods/KILabel/KILabel/Source/KILabel.m
+++ iOSProject/Pods/KILabel/KILabel/Source/KILabel.m
@@ -433,7 +433,7 @@ NSString * const KILabelLinkKey = @"link";
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSError *error = nil;
-        regex = [[NSRegularExpression alloc] initWithPattern:@"(?<!\\w)#([\\w\\_]+)?" options:0 error:&error];
+        regex = [[NSRegularExpression alloc] initWithPattern:@"#.*?#" options:0 error:&error];
     });
     
     // Run the expression and get matches
