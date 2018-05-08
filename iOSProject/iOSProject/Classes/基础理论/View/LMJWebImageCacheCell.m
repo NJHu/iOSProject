13196e1d111ea9cab9d47f560fdc5ba29ffef024
胡鹏 <64hp@163.com>
2018-05-08 13:41:36 +0800

    iOS系统已经解决了圆角造成的卡顿, 大家放心用吧.

--- iOSProject/iOSProject/Classes/基础理论/View/LMJWebImageCacheCell.m
+++ iOSProject/iOSProject/Classes/基础理论/View/LMJWebImageCacheCell.m
@@ -14,4 +14,25 @@
 @implementation LMJWebImageCacheCell
 
 
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self setupUIOnce];
+    }
+    
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self setupUIOnce];
+}
+
+- (void)setupUIOnce
+{
+    self.videoImageView.layer.cornerRadius = 20;
+    self.videoImageView.layer.masksToBounds = YES;
+}
+
 @end
